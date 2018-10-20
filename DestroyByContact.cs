79bc5c19594e6902f91ad99cbe5ecae8f834b8c6
Jacob Liebenow <liebenow.jacob@gmail.com>
2018-10-20 19:05:56 -0400

    Initial commit

---
+++ DestroyByContact.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController gameController;
+
+    private void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        } else
+        {
+            Debug.Log("Cannot find 'GameController' script.");
+        }
+        
+    }
+
+    // Use this for initialization
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boundary") || other.CompareTag("Enemy"))
+        {
+            return;
+        }
+        //Only make an explosion if there's some sort of explosion attached (determines if it's a bolt firing or an asteroid)
+        if (explosion != null) {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
+
+        gameController.AddScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ GameController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public Text scoreText;
+    public Text restartText;
+    public Text gameOverText;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+    // Use this for initialization
+    void Start() {
+        score = 0;
+        restartText.text = "";
+        gameOverText.text = "";
+        StartCoroutine (SpawnWaves());
+        gameOver = false;
+        restart = false;
+        UpdateScore();
+        
+    }
+
+    private void Update()
+    {
+        if (restart)
+        {
+            if(Input.GetKeyDown (KeyCode.R))
+            {
+                SceneManager.LoadScene("Main");
+            }
+        }
+    }
+    IEnumerator SpawnWaves () {
+
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                SpawnAsteroid();
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if(gameOver)
+            {
+                restartText.text = "Press 'R' to Restart";
+                restart = true;
+                break;
+            }
+        }
+	}
+
+    void SpawnAsteroid ()
+    {
+        GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+        Quaternion spawnRotation = Quaternion.identity;
+        Instantiate(hazard, spawnPosition, spawnRotation);
+    }
+
+    //Updates the int score value upon the destruction of an asteroid, and updates the GUI to display this
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        restartText.text = "Waiting...";
+        gameOver = true;
+    }
+    
+}
